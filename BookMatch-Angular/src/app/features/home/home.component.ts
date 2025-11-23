09565caa9f593abf8d3877e7bf42fc6487a0c1c4
Sergii <sergiibutrii555@gmail.com>
2025-11-23 13:03:20 +0100

    feat: se creado el componente de footer y añadido a la página home, ajustes generales para mayor responsividad y cambios en el componente home como añadir un banner al principio.

--- BookMatch-Angular/src/app/features/home/home.component.ts
+++ BookMatch-Angular/src/app/features/home/home.component.ts
@@ -4,11 +4,12 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 import { Header } from '@shared/components/header/header';
 import { Carousel } from '@shared/components/carousel/carousel';
+import { Footer } from '@shared/components/footer/footer';
 
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, Header, Carousel],
+  imports: [CommonModule, Header, Carousel, Footer],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
