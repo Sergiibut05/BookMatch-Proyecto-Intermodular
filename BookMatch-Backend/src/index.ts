ffa9128520ce3dd78407ed0b1a70cd39b3dd6533
Sergii <sergiibutrii555@gmail.com>
2025-12-17 23:14:58 +0100

    Fix: Configurar backend para Vercel serverless functions

--- BookMatch-Backend/src/index.ts
+++ BookMatch-Backend/src/index.ts
@@ -2,24 +2,30 @@ import app from './app.js';
 import { env } from './config/env.js';
 import { logger } from './utils/logger.js';
 
-const server = app.listen(env.PORT, () => {
-  logger.info(`API escuchando en http://localhost:${env.PORT}`);
-  logger.info(`Entorno: ${env.NODE_ENV}`);
-});
+// Para Vercel: exportar el handler
+export default app;
 
-process.on('SIGTERM', () => {
-  logger.info('SIGTERM recibido, cerrando servidor...');
-  server.close(() => {
-    logger.info('Servidor cerrado');
-    process.exit(0);
+// Para desarrollo local: iniciar el servidor
+if (process.env.NODE_ENV !== 'production' || process.env.VERCEL !== '1') {
+  const server = app.listen(env.PORT, () => {
+    logger.info(`API escuchando en http://localhost:${env.PORT}`);
+    logger.info(`Entorno: ${env.NODE_ENV}`);
   });
-});
 
-process.on('SIGINT', () => {
-  logger.info('SIGINT recibido, cerrando servidor...');
-  server.close(() => {
-    logger.info('Servidor cerrado');
-    process.exit(0);
+  process.on('SIGTERM', () => {
+    logger.info('SIGTERM recibido, cerrando servidor...');
+    server.close(() => {
+      logger.info('Servidor cerrado');
+      process.exit(0);
+    });
   });
-});
+
+  process.on('SIGINT', () => {
+    logger.info('SIGINT recibido, cerrando servidor...');
+    server.close(() => {
+      logger.info('Servidor cerrado');
+      process.exit(0);
+    });
+  });
+}
 
